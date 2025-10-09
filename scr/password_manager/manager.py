175a715d7a255c75551e60e6dee7b0d886102900
LucaBarilArbour <2563230@cegepoutaouais.qc.ca>
2025-10-08 21:04:07 -0400

    Fixed: typo

--- scr/password_manager/manager.py
+++
@@ -1,7 +0,0 @@
-def master_password(mdp):
-    """
-    Responsable de la création de master password et la validation du master password
-    :param mdp: Le mot de passe de l'utilisateur:
-    :return: le mot de passe hashed
-    """
-
---
+++ src/password_manager/manager.py
@@ -0,0 +1,23 @@
+import os, json, sys, hashlib 
+from prompt_toolkit import prompt
+from prompt_toolkit.document import Document
+from prompt_toolkit.validation import Validator, ValidationError
+
+def creation_master_password():
+    mot_de_passe = input("Entré un mot de passe: ")
+    if len(mot_de_passe) <= 12:
+        confirmation_mot_de_passe_faible = input("Votre mot de passe est faible.\n Voulez vous continuer avec un mot de passe faible?(y/n): ")
+        if confirmation_mot_de_passe_faible == "y":
+
+    mot_de_passe_a_confirmer = input("Confirmer le mot de passe: ")
+
+    if mot_de_passe == mot_de_passe_a_confirmer:
+        mot_de_passe_confirmer = mot_de_passe
+    else:
+        valide = False
+        while valide == False:
+            print("mot de passe")
+
+
+    
+
